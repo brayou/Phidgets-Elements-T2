8279a1d55e3690cef476c0b362ff979bcc09d5d8
Branden Youssef <brandenyoussef@gmail.com>
2012-07-31 21:14:40 -0400

    (Branden)Spawned the orbs and retextured them
    Did not position all of them yet, only the first in each circle

--- petri.py
+++ petri.py
@@ -65,57 +65,7 @@ class World(DirectObject):
         self.robot.setPosHpr(0,0,-.025,0,0,0)
         self.robot.setScale(0.0025)
         ## self.robot.place()
-        # Load the mana orbs
-        ## self.mana = []
-        ## for i in range(NUM_RINGS):
-            ## for j in range(NUM_ORBS):
-        self.mana = (loader.loadModel("Models/petridish.egg"))
-        self.mana.reparentTo(render)
-        self.manaTex = loader.loadTexture("Textures/HEX_Blue.jpg")
-        self.mana.setTexture(self.manaTex,1)
-        self.mana.setPos(-self.deltaFunc(0.7019,3.0,0.875),-.05,self.deltaFunc(1.072,3.0,0.875))
-        self.mana.setHpr(0,0,0)
-        self.mana.setScale(0.00005)
         
-        self.mana2 = (loader.loadModel("Models/petridish.egg"))
-        self.mana2.reparentTo(render)
-        self.manaTex = loader.loadTexture("Textures/HEX_Green.jpg")
-        self.mana2.setTexture(self.manaTex,1)
-        self.mana2.setPos(-self.deltaFunc(2.066,2.5,0.75),-.05,self.deltaFunc(0.03798,2.5,0.75))
-        self.mana2.setHpr(0,0,0)
-        self.mana2.setScale(0.00005)
-        
-        self.mana3 = (loader.loadModel("Models/petridish.egg"))
-        self.mana3.reparentTo(render)
-        self.manaTex = loader.loadTexture("Textures/HEX_Red.jpg")
-        self.mana3.setTexture(self.manaTex,1)
-        self.mana3.setPos(-self.deltaFunc(3.0,2.0,0.625),-.05,self.deltaFunc(0.2679,2.0,0.625))
-        self.mana3.setHpr(0,0,0)
-        self.mana3.setScale(0.00005)
-        
-        self.mana4 = (loader.loadModel("Models/petridish.egg"))
-        self.mana4.reparentTo(render)
-        self.manaTex = loader.loadTexture("Textures/HEX_Yellow.jpg")
-        self.mana4.setTexture(self.manaTex,1)
-        self.mana4.setPos(-self.deltaFunc(2.91,1.5,0.5),-.05,self.deltaFunc(0.987,1.5,0.5))
-        self.mana4.setHpr(0,0,0)
-        self.mana4.setScale(0.00005)
-                            
-        self.mana5 = (loader.loadModel("Models/petridish.egg"))
-        self.mana5.reparentTo(render)
-        self.manaTex = loader.loadTexture("Textures/HEX_Red.jpg")
-        self.mana5.setTexture(self.manaTex,1)
-        self.mana5.setPos(-self.deltaFunc(1.94,1.0,0.375),-.05,-self.deltaFunc(0.658,1.0,0.375))
-        self.mana5.setHpr(0,0,0)
-        self.mana5.setScale(0.00005)
-        
-        self.mana6 = (loader.loadModel("Models/petridish.egg"))
-        self.mana6.reparentTo(render)
-        self.manaTex = loader.loadTexture("Textures/HEX_Green.jpg")
-        self.mana6.setTexture(self.manaTex,1)
-        self.mana6.setPos(-self.deltaFunc(0.75,.5,0.25),-.05,-self.deltaFunc(0.067,.5,0.25))
-        self.mana6.setHpr(0,0,0)
-        self.mana6.setScale(0.00005)
         
         
     def deltaFunc(self,v,r,mult):
@@ -124,6 +74,93 @@ class World(DirectObject):
         V = R*mult-((R*mult*v)/r)
         print V
         return V
+        
+    ## def phaseOne(self):
+        
+    def phaseTwo(self):
+        # Load the mana orbs
+        self.mana = [None] * 33
+        self.manaTex = {"earth" :  loader.loadTexture("Textures/HEX_Green.jpg"),
+                           "wind" : loader.loadTexture("Textures/HEX_Yellow.jpg"),
+                           "fire" : loader.loadTexture("Textures/HEX_Red.jpg"),
+                           "water" : loader.loadTexture("Textures/HEX_Blue.jpg") }
+        # Spawn all of the mana orbs
+        for i in xrange(33):
+            # 0 of each element is currently in place
+            self.earthCount = 0
+            self.windCount = 0
+            self.fireCount = 0
+            self.waterCount = 0
+            # Load the mana models and scale them
+            self.mana[i] = (loader.loadModel("Models/petridish.egg"))
+            self.mana[i].reparentTo(render)
+            self.mana[i].setHpr(0,0,0)
+            self.mana[i].setScale(0.00005)
+            randElem = randint(0,3)
+            # If [element] and less than 2 [element] mana in the current circle
+                # texture the mana orb
+            if randElem == 0 and self.earthCount < 2:
+                self.manaTex = loader.loadTexture(self.manaTex["earth"])
+                self.mana[i].setTexture(self.manaTex,1)
+                self.earthCount += 1
+            elif randElem == 1 and self.windCount < 2:
+                self.manaTex = loader.loadTexture(self.manaTex["fire"])
+                self.mana[i].setTexture(self.manaTex,1)
+                self.windCount += 1
+            elif randElem == 2 and self.fireCount < 2:
+                self.manaTex = loader.loadTexture(self.manaTex["water"])
+                self.mana[i].setTexture(self.manaTex,1)
+                self.fireCount += 1
+            elif randElem == 3 and self.waterCount < 2:
+                self.manaTex = loader.loadTexture(self.manaTex["air"])
+                self.mana[i].setTexture(self.manaTex,1)
+                self.waterCount += 1
+            # Reset the element counts when moving onto the next circle so as to allow 2 per
+            if i == 8 or i == 15 or i == 21 or i == 26 or i == 30:
+                self.earthCount = 0
+                self.windCount = 0
+                self.fireCount = 0
+                self.airCount = 0
+                
+        # 6th circle positioning
+        self.mana[0].setPos(-self.deltaFunc(0.7019,3.0,0.875),-.05,self.deltaFunc(1.072,3.0,0.875))
+        self.mana[1].setPos(0,-0.5,0)
+        self.mana[2].setPos(0,-0.5,0)
+        self.mana[3].setPos(0,-0.5,0)
+        self.mana[4].setPos(0,-0.5,0)
+        self.mana[5].setPos(0,-0.5,0)
+        self.mana[6].setPos(0,-0.5,0)
+        self.mana[7].setPos(0,-0.5,0)
+        # 5th circle positioning
+        self.mana[8].setPos(-self.deltaFunc(2.066,2.5,0.75),-.05,self.deltaFunc(0.03798,2.5,0.75))
+        self.mana[9].setPos(0,-0.5,0)
+        self.mana[10].setPos(0,-0.5,0)
+        self.mana[11].setPos(0,-0.5,0)
+        self.mana[12].setPos(0,-0.5,0)
+        self.mana[13].setPos(0,-0.5,0)
+        self.mana[14].setPos(0,-0.5,0)
+        # 4th circle positioning
+        self.mana[15].setPos(-self.deltaFunc(3.0,2.0,0.625),-.05,self.deltaFunc(0.2679,2.0,0.625))
+        self.mana[16].setPos(0,-0.5,0)
+        self.mana[17].setPos(0,-0.5,0)
+        self.mana[18].setPos(0,-0.5,0)
+        self.mana[19].setPos(0,-0.5,0)
+        self.mana[20].setPos(0,-0.5,0)
+        # 3th circle positioning
+        self.mana[21].setPos(-self.deltaFunc(2.91,1.5,0.5),-.05,self.deltaFunc(0.987,1.5,0.5))
+        self.mana[22].setPos(0,-0.5,0)
+        self.mana[23].setPos(0,-0.5,0)
+        self.mana[24].setPos(0,-0.5,0)
+        self.mana[25].setPos(0,-0.5,0)
+        # 2nd circle positioning
+        self.mana[26].setPos(-self.deltaFunc(1.94,1.0,0.375),-.05,-self.deltaFunc(0.658,1.0,0.375))
+        self.mana[27].setPos(0,-0.5,0)
+        self.mana[28].setPos(0,-0.5,0)
+        self.mana[29].setPos(0,-0.5,0)
+        # 1st circle positioning
+        self.mana[30].setPos(-self.deltaFunc(0.75,.5,0.25),-.05,-self.deltaFunc(0.067,.5,0.25))
+        self.mana[31].setPos(0,-0.5,0)
+        self.mana[32].setPos(0,-0.5,0)
         
         # Quit Game Function
     def quit(self):
